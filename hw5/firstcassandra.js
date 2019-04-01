961ac03179d11096566c155c750a957e7829b6b8
dsli208 <ds.li@outlook.com>
2019-04-01 07:10:44 -0400

    Investigate how to get filename

--- hw5/firstcassandra.js
+++ hw5/firstcassandra.js
@@ -4,6 +4,9 @@ const port = 3000;
 
 var cassandra = require('cassandra-driver');
 
+app.use(express.urlencoded());
+app.use(express.json());
+
 const client = new cassandra.Client({
   contactPoints: ['host1'],
   localDataCenter: 'datacenter1'
@@ -16,7 +19,7 @@ client.connect(function (err) {
 app.get('/', (req, res) => res.send("Listening"))
 
 app.post('/deposit', (req, res) => {
-
+  console.log(req.body.user.filename);
   const query = 'INSERT INTO imgs (filename, contents) VALUES (%s, %s)';
   const params = [req.body.filename, req.body.contents];
   client.execute(query, params, { prepare: true }, function (err) {
