4bbd02863ce7bb916644e9f87e1e18e5818a555d
dsli208 <ds.li@outlook.com>
2019-04-01 17:53:43 -0400

    Add read file block

--- hw5/firstcassandra.js
+++ hw5/firstcassandra.js
@@ -10,6 +10,8 @@ app.use(express.json());
 var multer  = require('multer')
 var upload = multer({dest: 'uploads/'})
 
+var fs = require('file-system')
+
 const client = new cassandra.Client({
   contactPoints: ['127.0.0.1'],
   localDataCenter: 'datacenter1',
@@ -26,8 +28,21 @@ app.get('/', upload.none(), (req, res, next) => res.send("Listening"))
 app.post('/deposit', upload.single('contents'), (req, res, next) => {
   console.log(req.body);
   console.log(req.file.buffer);
+
+  // Extract data from file
+  var content;
+  fs.readFile(req.file, function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    content = data;
+
+    // Invoke the next step here however you like
+    console.log(content);   // Put all of the code here (not the best solution)
+  });
+
   const query = 'INSERT INTO imgs (filename, contents) VALUES (?, ?)';
-  const params = [req.body.filename, req.file.buffer];
+  const params = [req.body.filename, content];
   client.execute(query, params, { prepare: true }, function (err) {
     console.log(err); // if no error, undefined
     console.log("Inserted into Cluster?");
