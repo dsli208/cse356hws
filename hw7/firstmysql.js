0b8f7328d142faee89dfcd6916d76efca812e5c7
dsli208 <ds.li@outlook.com>
2019-04-10 22:59:54 -0400

    Rearrange queries

--- hw7/firstmysql.js
+++ hw7/firstmysql.js
@@ -20,6 +20,8 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
+var q1 = "SELECT club, pos, player, a FROM assists WHERE `pos` = ? and `club` = ? ORDER BY a DESC, gs DESC, player";
+var q2 = "SELECT AVG(A) AS average FROM assists WHERE `pos` = ? and `club` = ?";
 
 app.get('/hw7', (req, res) => {
 
@@ -31,11 +33,11 @@ app.get('/hw7', (req, res) => {
   mcclient.get(key, function(err3, res3) {
     if (err3) {
       // Find the club + pos from DB
-      connection.query("SELECT club, pos, player, a FROM assists WHERE `pos` = ? and `club` = ? ORDER BY a DESC, gs DESC, player", [pos, club], function(err1, res1) {
+      connection.query(q1, [pos, club], function(err1, res1) {
           console.log(res1[0]);
 
           // Get the average assists
-          connection.query("SELECT AVG(A) AS average FROM assists WHERE `pos` = ? and `club` = ?", [pos, club], function(err2, res2) {
+          connection.query(q2, [pos, club], function(err2, res2) {
             console.log(res2[0]);
             res.json({"club": res1[0].club, "pos": res1[0].pos, "max_assists": res1[0].a, "player": res1[0].player, "avg_assists": res2[0].average});
             //mcclient.set(key, JSON.stringify({"club": res1[0].club, "pos": res1[0].pos, "max_assists": res1[0].a, "player": res1[0].player, "avg_assists": res2[0].average}));
