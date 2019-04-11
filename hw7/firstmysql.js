d14929c830066d987d36709fa5a3a755197948b8
dsli208 <ds.li@outlook.com>
2019-04-10 22:44:10 -0400

    Fix bugs with mcclient comments out

--- hw7/firstmysql.js
+++ hw7/firstmysql.js
@@ -24,11 +24,11 @@ connection.connect();
 app.get('/hw7', (req, res) => {
 
   // Get query vars
-  /*var club = req.query.club + "";
+  var club = req.query.club + "";
   var pos = req.query.pos + "";
   club = club.toUpperCase(); pos = pos.toUpperCase();
   var key = club + " " + pos;
-  mcclient.get(key, function(err3, res3) {
+  /*mcclient.get(key, function(err3, res3) {
     if (err3) {*/
       // Find the club + pos from DB
       connection.query("SELECT club, pos, player, a FROM assists WHERE `pos` = ? and `club` = ? ORDER BY a DESC, gs DESC, player", [pos, club], function(err1, res1) {
@@ -38,7 +38,7 @@ app.get('/hw7', (req, res) => {
           connection.query("SELECT AVG(A) AS average FROM assists WHERE `pos` = ? and `club` = ?", [pos, club], function(err2, res2) {
             console.log(res2[0]);
             res.json({"club": res1[0].club, "pos": res1[0].pos, "max_assists": res1[0].a, "player": res1[0].player, "avg_assists": res2[0].average});
-            mcclient.set(key, JSON.stringify({"club": res1[0].club, "pos": res1[0].pos, "max_assists": res1[0].a, "player": res1[0].player, "avg_assists": res2[0].average}));
+            //mcclient.set(key, JSON.stringify({"club": res1[0].club, "pos": res1[0].pos, "max_assists": res1[0].a, "player": res1[0].player, "avg_assists": res2[0].average}));
           })
       })
     /*}
